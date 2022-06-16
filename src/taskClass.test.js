49cbe2d06b061563308aef13ce19fe8f585d52e7
Desor Guishny <desorguishny@gmail.com>
2022-06-16 13:04:36 -0400

    Implement test for edit method

--- src/taskClass.test.js
+++ src/taskClass.test.js
@@ -12,3 +12,23 @@ describe('the add method', () => {
   });
 });
 
+//test Edit
+
+describe('the edit method', () => {
+  beforeEach(() => {
+    tasks = new Tasks();
+    tasks.addTask(1, 'Description', false);
+  });
+  it('should be able to update the task description', () =>{
+    let newvalue = 'New Description'; 
+    let oldValue = tasks.getTasks()[0][1];
+    tasks.updateDesc(0, newvalue);
+    expect(oldValue).not.toBe(newvalue);
+  });
+  it('the new value should be equal to the new description', () =>{
+    let newDescription = 'New Description'
+    tasks.updateDesc(0, newDescription);
+    let newValue = tasks.getTasks()[0][1];
+    expect(newValue).toBe(newDescription);
+  })
+})
